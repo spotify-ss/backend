5b768cec9879c94134b62ad987342fb4889064f8
Chris Cedeno <chrisrey95@gmail.com>
2019-03-08 15:59:16 -0500

    added 404 route

--- index.js
+++ index.js
@@ -7,4 +7,9 @@ server.get('/', (req, res) => {
     res.status(200).json({api: `Alive at port ${port}`})
 })
 
+
+
+server.get('*', (req, res) => {
+    res.status(404).json({message: "route not found"})
+})
 server.listen(port, () => console.log(`\nServer live on: ${port}\n`));
