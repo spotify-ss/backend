3e7dbd8930c43f4f731763cb474eb20ad4b4dfe9
Modesto Tamayo <modesto.t.tamayo@gmail.com>
2019-03-11 11:24:45 -0500

    Wrote out the end-point for registering a user, hashing the user password before it is added the the database and sending back a jsonwebtoken

---
+++ api/users-router.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const bcrypt = require('bcryptjs');
+
+const Users = require('../users/users-model.js');
+const tokenService = require('../auth/tokenService.js');
+
+const router = express.Router();
+
+router.post('/register', async (req, res) => {
+    if(!req.body.username || !req.body.password){
+        res.status(400).json({ error: 'Please input a Username and a Password' });
+    } else if(!req.body.password.length < 8){
+        res.status(400).json({ error: 'The Password must be at least 8 characters long'});
+    } else {
+        try {
+            let newUser = req.body;
+
+            const hash = bcrypt.hashSync(newUser.password, 12);
+
+            newUser.password = hash;
+
+            const user = await Users.addUser(newUser);
+            const token = tokenService.generateToken(user);
+
+            res.status(201).json({ user, token });
+
+        } catch(error) {
+            if(error.errno === 19){
+                res.status(400).json({ error: 'That username is already taken' });
+            } else {
+                res.status(500).json({ error: "Something bad happened! The user couldn't be added!" });
+            }
+        }
+    }
+});
